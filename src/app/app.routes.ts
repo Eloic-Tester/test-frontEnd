9de8045cef7a71c6b9b17e1ffe64a2e789a503aa
Eloic Rakoto-Arisoa <eloicmax994@gmail.com>
2025-03-06 13:51:45 +0300

    Commit new changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,7 @@ export const routes: Routes = [
 
     { path: 'utilisateur', component: UserListComponent },  // Route pour user-list
 
-    { path: 'edit-user/:id', component: EditUserComponent },  ];
+    // { path: 'edit-user/:id', component: EditUserComponent },
+  ];
 
     
